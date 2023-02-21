d3cfaab5428f086fb590df31c5396b50540cdddb
kevin11-code <kevinharia11@gmail.com>
2023-02-21 14:05:53 +0530

    fixed middleware issue

--- middleware.js
+++ middleware.js
@@ -3,19 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
   const { pathname } = req.nextUrl;
 
   // Allow the requests if the following is true...
   // 1) Its a request for next-auth session & provider fetching
   // 2) the token exists
-
-  if (pathname.includes("/api/auth") || token) {
+  if (pathname.startsWith("/_next")) return NextResponse.next();
+  if (pathname.startsWith("/api/auth") || token) {
     return NextResponse.next();
   }
   // Redirect them to login if they dont have token AND are requesting a protected route
-
-  // if (!token && pathname !== "/login") {
-  //   return NextResponse.rewrite(new URL("/login", req.url));
-  // }
+  if (!token && pathname !== "/login") {
+    return NextResponse.rewrite(new URL("/login", req.url));
+  }
+  return NextResponse.next();
 }

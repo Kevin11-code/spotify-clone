676f27131221db4cd892b5e17d2cd133180ecf05
kevin11-code <kevinharia11@gmail.com>
2023-02-21 19:59:36 +0530

    Resolved errors

--- components/Player.js
+++ components/Player.js
@@ -1,20 +1,18 @@
-import { SpeakerWaveIcon as VolumeDownIcon } from '@heroicons/react/24/outline';
-import { SpeakerWaveIcon as VolumeUpIcon } from "@heroicons/react/24/solid";
-import {
-  BackwardIcon,
-  PauseIcon,
-  PlayIcon,
-  ForwardIcon,
-  ArrowUturnLeftIcon,
-  ArrowsRightLeftIcon,
-} from '@heroicons/react/24/solid';
-import { debounce } from 'lodash';
-import { useSession } from 'next-auth/react';
-import { useCallback, useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
-import useSongInfo from '../hooks/useSongInfo';
-import useSpotify from '../hooks/useSpotify';
+import VolumeUpIcon from "@mui/icons-material/VolumeUpOutlined";
+import VolumeOffIcon from "@mui/icons-material/VolumeOffOutlined";
+import { PauseIcon } from "@heroicons/react/24/solid";
+import PlayIcon from "@mui/icons-material/PlayArrowRounded";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
+import useSpotify from "../hooks/useSpotify";
+import ShuffleIcon from "@mui/icons-material/ShuffleRounded";
+import RepeatIcon from "@mui/icons-material/RepeatRounded";
+import RepeatOneIcon from "@mui/icons-material/RepeatOneRounded";
+import ForwardIcon from "@mui/icons-material/SkipNextRounded";
+import BackwardIcon from "@mui/icons-material/SkipPreviousRounded";
 
 function Player() {
   const spotifyApi = useSpotify();
@@ -23,8 +21,16 @@ function Player() {
     useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
   const [volume, setVolume] = useState(50);
+  const [shuffle, toggleShuffle] = useState(false);
+  const [repeat, setRepeat] = useState(0);
 
   const songInfo = useSongInfo(currentTrackId);
+
+  const toggleRepeat = () => {
+    const repeatArr = ["context", "track", "off"];
+    setRepeat((prev) => (prev + 1) % 3);
+    spotifyApi.setRepeat(repeatArr[repeat]);
+  };
 
   const fetchCurrentSong = () => {
     if (!songInfo) {
@@ -56,16 +62,9 @@ function Player() {
     }
   }, [currentTrackId, spotifyApi, session]);
 
-  const debouncedAdjustVolume = useCallback(
-    debounce((volume) => {
-      spotifyApi.setVolume(volume).catch((err) => {});
-    }, 300),
-    []
-  );
-
   useEffect(() => {
-    if (volume > 0 && volume < 100) {
-      debouncedAdjustVolume(volume);
+    if (volume >= 0 && volume <= 100) {
+      spotifyApi.setVolume(volume);
     }
   }, [volume]);
 
@@ -85,13 +84,17 @@ function Player() {
         </div>
       </div>
       <div className="flex items-center justify-evenly">
-        <ArrowsRightLeftIcon
-          onClick={() => spotifyApi.setShuffle()}
-          className="button"
+        <ShuffleIcon
+          onClick={() => {
+            spotifyApi.setShuffle(shuffle);
+            toggleShuffle((prev) => !prev);
+          }}
+          className={shuffle ? "button" : "button text-green-400"}
         />
+
         <BackwardIcon
           onClick={() => spotifyApi.skipToPrevious()}
-          className="button"
+          className="button h-8 w-8"
         />
         {isPlaying ? (
           <PauseIcon
@@ -99,34 +102,56 @@ function Player() {
             onClick={handlePlayPause}
           />
         ) : (
-          <PlayIcon className="button h-10 w-10" onClick={handlePlayPause} />
+          <PlayIcon className="button h-12 w-12" onClick={handlePlayPause} />
         )}
         <ForwardIcon
-          className="button"
+          className="button h-8 w-8"
           onClick={() => spotifyApi.skipToNext()}
         />
-        <ArrowUturnLeftIcon
-          className="button"
-          onClick={() => spotifyApi.setRepeat()}
-        />
+        <div>
+          {repeat === 0 ? (
+            <RepeatIcon className="button" onClick={toggleRepeat} />
+          ) : repeat === 1 ? (
+            <RepeatIcon
+              className="button text-green-400"
+              onClick={toggleRepeat}
+            />
+          ) : (
+            <RepeatOneIcon
+              className="button text-green-400"
+              onClick={toggleRepeat}
+            />
+          )}
+        </div>
       </div>
 
       <div className="flex items-center space-x-3 md:space-x-4 justify-end p-5">
-        <VolumeDownIcon
+        {/* <VolumeDownIcon
           className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
           onClick={() => volume > 0 && setVolume(volume - 10)}
-        />
+        /> */}
+        {volume ? (
+          <div
+            className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+            onClick={() => setVolume(0)}
+          >
+            <VolumeUpIcon />
+          </div>
+        ) : (
+          <div
+            className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+            onClick={() => setVolume(50)}
+          >
+            <VolumeOffIcon />
+          </div>
+        )}
         <input
           type="range"
           value={volume}
           onChange={(e) => setVolume(Number(e.target.value))}
           min={0}
           max={100}
-          className="w-14 md:w-36 "
-        />
-        <VolumeUpIcon
-          className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
-          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="range w-[90px] md:w-36 h-[3px] cursor-pointer rounded-3xl border-none"
         />
       </div>
     </div>
